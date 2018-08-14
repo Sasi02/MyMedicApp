a86e3cb8314c4f94a13a4f8421d01add7d599cae
sasi02 <hksasindu@02>
2018-08-14 20:57:10 +0530

    String Added

--- app/src/main/java/com/example/user/mymedic/PatientHome.java
+++ app/src/main/java/com/example/user/mymedic/PatientHome.java
@@ -1,8 +1,5 @@
 package com.example.user.mymedic;
 
-import android.app.FragmentManager;
-import android.app.FragmentTransaction;
-import android.content.res.Configuration;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
@@ -11,19 +8,8 @@ public class PatientHome extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_patient_home);
 
-        //this activity will be called for Top Bar when this page created
-        //Below code segment is used for include the Top Bar to this Page.
-        Configuration config = getResources().getConfiguration();
 
-        FragmentManager fragmentManager = getFragmentManager();
-        FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
-
-        //Creating a Patient Top Bar Object
-        PatientTopBar patientTopBar = new PatientTopBar();
-        fragmentTransaction.replace(android.R.id.content, patientTopBar);
-
-        //Commiting Fragment
-        fragmentTransaction.commit();
     }
 }
