fbe9727bbd67c3bd3b322b8e16f8eb554519a817
Lahiru <lahiru.senevi97@gmail.com>
2018-08-14 19:55:55 +0530

    Patient Profile, Two Sign Ups Created

---
+++ app/src/main/java/com/example/user/mymedic/PatientLogin.java
@@ -0,0 +1,14 @@
+package com.example.user.mymedic;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class PatientLogin extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_patient_login);
+
+    }
+}
