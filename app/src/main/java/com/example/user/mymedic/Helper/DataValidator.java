516269ec94f4939c4d193c1365969fc16d9d757c
Sachith Rukshan <sachithrukshanmail@gmail.com>
2018-09-27 13:18:37 +0530

    Work On Data Validator and Type Converter

--- app/src/main/java/com/example/user/mymedic/Helper/DataValidator.java
+++ app/src/main/java/com/example/user/mymedic/Helper/DataValidator.java
@@ -5,4 +5,23 @@ package com.example.user.mymedic.Helper;
  */
 
 public class DataValidator {
+    public static boolean isWord(String word){
+        return word.matches("^[a-zA-Z]+$");
+    }
+
+    public static boolean isPhoneNumber(String phone){
+        return phone.matches("^([\+]?94)?[0]?[1-9][0-9]{8}$");
+    }
+
+    public static boolean isNIC(String nic){
+        return nic.matches("^([0-9]{4}[0-8][0-9]{7})$|^([4-9][0-9][0-8][0-9]{6}[vV])$");
+    }
+
+    public static boolean isBlood(String blood){
+        return blood.matches("^[ABO][B]?[+-]$");
+    }
+
+    public static boolean isText(String text){
+        return text.matches("^[A-Za-z ]$");
+    }
 }
